d459b49ca4399506494fa7c37572189ec681bfbe
feibi-allen <146375798+feibi-allen@users.noreply.github.com>
2024-04-15 15:10:17 +0000

    arg check

--- mazeGen.c
+++ mazeGen.c
@@ -6,15 +6,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define MIN_WIDTH 5
 #define MAX_WIDTH 100
 #define MIN_HEIGHT 5
 #define MAX_HEIGHT 100
 
+#define EXIT_SUCCESS 0
+#define EXIT_ARG_ERROR 1
+
 typedef struct __Maze
 {
-    char *completeMaze;
+    char *grid;
 } maze;
 
 typedef struct __item
@@ -27,29 +31,48 @@ typedef struct __item
 void push(item **head, int cell)
 {
     item *tmp = (item*)malloc(sizeof(item));
+    tmp->cell=cell;
     tmp->previous= *head;
     (*head) = tmp;
-    tmp->cell=cell;
 }
 
 int pop(item **head){
     if (*head == NULL){
         return -1;
     }
-    printf("%d\n",(*head)->cell);
+    int cell = (*head)->cell;
     item *tmp = *head;
     *head = (*head)->previous;
     free(tmp);
+    return cell;
 }
 
-int main() {
-    item *head = (item*)malloc(sizeof(item));
-    head->cell=0;
-    head->previous=NULL;
-    push(&head,1);
-    printf("%d",head->cell);
-
+int main(int argc, char *argv[]) {
     // error check args
+    if (argc !=4){
+        printf("Usage: ./maze <filename>\n");
+        return 1;
+    }
+
+    int height = atoi(argv[3]);
+    int width = atoi(argv[2]);
+
+    if (height<MIN_HEIGHT||height>MAX_HEIGHT||width<MIN_WIDTH||width>MAX_WIDTH){
+        printf("Usage: ./maze <filename>\n");
+        return 1;
+    }
+
+    // asign maze struct grid memory
+    maze mz;
+    mz.grid = (char *)malloc(atoi(argv[2])*atoi(argv[3])*sizeof(char));
+
+    //initialize stack
+    item *head = NULL;
+    for (int i=0;i<10;i++){
+        push(&head,i);
+        printf("%d",pop(&head));
+    }
+
     // asign maze struct grid memory
     // fill grid with walls
     // using working maze:
